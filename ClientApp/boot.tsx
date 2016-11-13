f9f648353d8e99d257b47dc27eb36ba95afe9f8d
Szymon <szymon.gwozdz@outlook.com>
2016-11-13 16:36:47 +0100

    initial react components

---
+++ ClientApp/boot.tsx
@@ -0,0 +1,14 @@
+import './css/site.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes';
+
+//This code starts up the React app. 
+//Sets up the routing configuration
+//Injects the app into DOM element
+ReactDOM.render(
+  <Router history={browserHistory} children={routes}/>,
+  document.getElementById('react-app')  
+);
---
+++ ClientApp/routes.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Router, Route, HistoryBase } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+
+export default <Route component={Layout}>
+    <Route path='/' components={{ body: Home }} />
+</Route>
+
+
+//For WebPack
+declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+}
