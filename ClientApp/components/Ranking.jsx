a840e0accb44c4d9e965e99e71986dc13e999221
Szymon <szymon.gwozdz@outlook.com>
2016-12-03 15:52:11 +0100

    connect to webapi

--- ClientApp/components/Ranking.jsx
+++ ClientApp/components/Ranking.jsx
@@ -1,32 +1,24 @@
 import * as React from 'react';
 import { RankingElement } from './RankingElement';
 import * as $ from 'jquery';
+import * as axios from 'axios';
 
 export class Ranking extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            albums: []
+        }
+    }
+    
+    componentDidMount() {
+    $.post("http://localhost:4000/graphql", {query: "{albums {title}}"}, function(response) {
+        this.setState({albums: response.data.albums})
+         }.bind(this), "json");
+    }
+    
     render() {
-        // var xhr = new XMLHttpRequest();
-        // xhr.responseType = 'jsonp';
-        // xhr.open("POST", "http://localhost:4000/graphql");
-        // xhr.setRequestHeader("Content-Type", "application/json");
-        // xhr.setRequestHeader("Accept", "application/json");
-        // xhr.onload = function () {
-
-        // }
-        // xhr.send(JSON.stringify({ query: "{albums {title}}" }));
-
-        $.ajax({ // ajax call starts
-            crossDomain: true,
-            type: "POST",
-            url: 'http://localhost:4000/graphql', // JQuery loads areas
-            data: { query: "{albums {title}}" },
-            dataType: 'jsonp', // Choosing a JSON datatype
-            async: false,
-            success: function (data) // Variable data contains the data we get from serverside
-            {
-                this.props.albums = data;
-            }
-        });
-
         return <table className="table table-hover">
             <thead>
                 <tr>
@@ -36,7 +28,7 @@ export class Ranking extends React.Component {
                 </tr>
             </thead>
             <tbody>
-                {this.props.albums.map(album =>
+              {this.state.albums.map(album =>
                     <RankingElement album={album} />)}
             </tbody>
         </table>;
--- ClientApp/components/RankingElement.jsx
+++ ClientApp/components/RankingElement.jsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 export class RankingElement extends React.Component{ 
     render() {
         return <tr>
-            <th>this.props.album.place</th>
-            <th>this.props.album.title</th>
-            <th>this.props.album.grade</th>
+            <th>1</th>
+            <th>{this.props.album.title}</th>
+            <th>1.2</th>
         </tr>;
     }
 }
