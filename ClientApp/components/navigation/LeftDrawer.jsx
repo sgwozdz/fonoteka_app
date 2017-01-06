8a5d1bfbdd1367887894e27547fc38f5548e0788
Szymon <szymon.gwozdz@outlook.com>
2017-01-06 09:38:09 +0100

    refactor left drawer

---
+++ ClientApp/components/navigation/LeftDrawer.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import {Drawer,MenuItem, Divider} from 'material-ui';
+
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
+export class LeftDrawer extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: this.props.open
+        };
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClose(){
+        this.setState({
+            open: false
+        });
+        this.props.onChange();
+    }
+
+    componentWillReceiveProps(nextProps)
+    {
+        this.setState({
+            open: nextProps.open
+        })
+    }
+    render() {
+        let LoggedMenu = <div> 
+                            <Divider />
+                            <MenuItem containerElement={<Link to="/friends"/>} onTouchTap={this.handleClose}>Znajomi</MenuItem>
+                            <MenuItem containerElement={<Link to="/messages"/>} onTouchTap={this.handleClose}>Wiadomości</MenuItem>
+                            <Divider />
+                            <MenuItem containerElement={<Link to="/settings"/>} onTouchTap={this.handleClose}>Ustawienia</MenuItem>
+                        </div>;
+        return (
+            <Drawer open={this.state.open} docked={false} onRequestChange={(open) => this.setState({open})}>
+                <div>
+                    <MenuItem containerElement={<Link to="/"/>} onTouchTap={this.handleClose}>Aktualności</MenuItem>
+                    <MenuItem containerElement={<Link to="/base"/>} onTouchTap={this.handleClose}>Baza</MenuItem>
+                    <MenuItem containerElement={<Link to="/ranking"/>} onTouchTap={this.handleClose}>Ranking</MenuItem>
+                    <MenuItem containerElement={<Link to="/events"/>} onTouchTap={this.handleClose}>Wydarzenia</MenuItem>
+                </div>
+                {LoggedMenu}
+            </Drawer>
+        );
+    }
+}
--- ClientApp/components/navigation/NavAppBar.jsx
+++ ClientApp/components/navigation/NavAppBar.jsx
@@ -1,10 +1,7 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
-import {AppBar, Drawer, MenuItem, Divider} from 'material-ui';
+import {AppBar} from 'material-ui';
 import {RightMenu} from './RightMenu';
-
-var injectTapEventPlugin = require("react-tap-event-plugin");
-injectTapEventPlugin();
+import {LeftDrawer} from './LeftDrawer';
 
 export class NavAppBar extends Component {
     constructor(props) {
@@ -12,9 +9,7 @@ export class NavAppBar extends Component {
         this.state = {
             open: false
         };
-
         this.handleToggle = this.handleToggle.bind(this);
-        this.handleClose = this.handleClose.bind(this);
     }
 
     handleToggle() {
@@ -23,12 +18,6 @@ export class NavAppBar extends Component {
         });
     }
     
-    handleClose(){
-        this.setState({
-            open: false
-        });
-    }
-
     render() {
         const appbarStyle = {
             backgroundColor: '#616161'
@@ -44,18 +33,8 @@ export class NavAppBar extends Component {
                     titleStyle={titleStyle}
                     style={appbarStyle}
                     onLeftIconButtonTouchTap={this.handleToggle}
-                    iconElementRight={<RightMenu logged = {this.props.logged}/>}/>
-                <Drawer open={this.state.open} docked={false} onRequestChange={(open) => this.setState({open})}>
-                    <MenuItem containerElement={<Link to="/"/>} onTouchTap={this.handleClose}>Aktualności</MenuItem>
-                    <MenuItem containerElement={<Link to="/base"/>} onTouchTap={this.handleClose}>Baza</MenuItem>
-                    <MenuItem containerElement={<Link to="/ranking"/>} onTouchTap={this.handleClose}>Ranking</MenuItem>
-                    <Divider />
-                    <MenuItem containerElement={<Link to="/friends"/>} onTouchTap={this.handleClose}>Znajomi</MenuItem>
-                    <MenuItem containerElement={<Link to="/messages"/>} onTouchTap={this.handleClose}>Wiadomości</MenuItem>
-                    <MenuItem containerElement={<Link to="/events"/>} onTouchTap={this.handleClose}>Wydarzenia</MenuItem>
-                    <Divider />
-                    <MenuItem containerElement={<Link to="/settings"/>} onTouchTap={this.handleClose}>Ustawienia</MenuItem>
-                </Drawer>
+                    iconElementRight={<RightMenu logged={this.props.logged}/>}/>
+                    <LeftDrawer logged={this.props.logged} open={this.state.open} onChange={this.handleToggle}/>
             </div>
         );
     }
