e0baaebac6d0633343958bc597c6d5b9c4052596
Szymon <szymon.gwozdz@outlook.com>
2017-01-07 23:12:29 +0100

    fix for news without author

--- ClientApp/components/card/NewsCard.jsx
+++ ClientApp/components/card/NewsCard.jsx
@@ -4,11 +4,23 @@ import {Card, CardActions, CardTitle, CardText, CardMedia} from 'material-ui/Car
 import FlatButton from 'material-ui/FlatButton';
 
 export class NewsCard extends Component {
+    constructor(props) {
+        super(props);
+        
+        var p = this.props.post;
+
+        this.state = {
+            title: p.title,
+            subtitle: p.author ? p.author.username : ''
+        }
+    }
+    
     render() {
+
         return (
             <Card>
                 <CardMedia
-                    overlay={<CardTitle title={this.props.post.title} subtitle={this.props.post.author.username}/>}>
+                    overlay={<CardTitle title={this.state.title} subtitle={this.state.subtitle}/>}>
                <img src={require('../ironman1.jpg')}/> 
                 </CardMedia>
                 <CardText>
